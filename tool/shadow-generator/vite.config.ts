f5172bcae9152b2f983a04d2aa2c88dacbe16144
achamaro <a.achamaro@gmail.com>
2023-05-06 21:56:45 +0900

    chore: pages

--- tool/shadow-generator/vite.config.ts
+++ tool/shadow-generator/vite.config.ts
@@ -4,11 +4,12 @@ import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [icon(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+  base: mode === "production" ? "/tailwindcss-shadow/" : "/",
+}));
